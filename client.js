da11c1b8ddc6de38564b3f1e7a8745a98058afe6
pthomas0309 <preston.thomas355@gmail.com>
2021-05-15 02:04:47 -0500

    esubmit button adds employee object to the employeeSheet array

--- client.js
+++ client.js
@@ -1,1 +1,19 @@
-console.log('js loaded')
+$(document).ready(readyNow);
+
+function readyNow(){
+    $('#submitBtn').on('click', appendEmployeeInfo)
+}
+
+let employeeSheet = []
+
+function appendEmployeeInfo(){
+    let employee = {
+        fName: $('#firstName').val(),
+        lName: $('#lastName').val(),
+        empID: $('#employeeId').val(),
+        jTitle: $('#jobTitle').val(),
+        aSalary: $('#annualSalary').val(),
+    }
+    employeeSheet.push(employee);
+    console.log(employeeSheet);
+}
